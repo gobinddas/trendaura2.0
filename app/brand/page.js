166f1f40743f98924e56af260bc815c7aec8e232
Gobind Das <gobind98077@gmail.com>
2025-06-28 01:12:37 +0545

    brand page added

--- app/brand/page.js
+++ app/brand/page.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Brandpage = () => {
   return (
-    <div>Aboutpage</div>
+    <div className='brand-page'>
+      <div className='container'>
+        <div className='brand-page-content'>
+          <h1 className='block-heading'>Trendaura: Where Fashion Meets Everyday Confidence</h1>
+          <p className='brand-des'>At Trendaura, we bring you trendy, comfortable, and affordable fashion that fits your lifestyle. We believe in self-expression through style and deliver pieces that empower your everyday look.</p>
+          <div className='brand-row'>
+            <p>
+             At Trendaura, we are deeply committed to providing clothing that reflects exceptional quality, comfort, and style. Every piece is crafted from premium fabrics chosen for their durability, feel, and performance. Our team of skilled designers constantly studies global fashion trends to ensure our collections stay modern, elegant, and relevant. <br/>  We blend timeless designs with contemporary aesthetics, creating clothing that flatters all body types and suits every occasion. Whether you're dressing for work, casual outings, or special events, our pieces are made to help you feel confident, stylish, and comfortable. Trendaura is where lasting quality meets everyday fashion with purpose.
+            </p>
+            <img src='./banner2.jpg' />
+          </div>
+
+
+
+
+           <p className='brand-des'>Looking ahead, Trendaura aims to expand into sustainable fashion and launch exclusive designer collaborations. We value customer feedback and strive to enhance your shopping experience through innovation, responsive support, and community-driven growth. Join us as we redefine modern style—one confident step at a time.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
